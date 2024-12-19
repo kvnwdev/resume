4e0c48490f7ffeea8d48c4160837664be0cb9f69
kvnwdev <47703820+kvnwdev@users.noreply.github.com>
2024-12-18 22:28:08 -0500

    fix: globals.css origin fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
-import "@/styles/globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
